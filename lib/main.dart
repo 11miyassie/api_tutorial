171a780372666a8ed103096d134dc91a08a3d233
11mrhappy <nekomamiremy@gmail.com>
2021-01-25 22:52:57 +0900

    convert the http.response to an album

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
+import 'dart:convert';
 
 void main() {
   runApp(MyApp());
@@ -22,8 +23,18 @@ class Album {
   }
 }
 
-Future<http.Response> fetchAlbum() {
-  return http.get('https://jsonplaceholder.typicode.com/albums/1');
+Future<Album> fetchAlbum() async {
+  final response = await http.get('https://jsonplaceholder.typicode.com/albums/1');
+
+  if (response.statusCode == 200) {
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+    return Album.fromJson(jsonDecode(response.body));
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to load album');
+  }
 }
 
 class MyApp extends StatelessWidget {
