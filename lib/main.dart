d06bbe68507a6b3e536f934d96f614f58b6e4605
11mrhappy <nekomamiremy@gmail.com>
2021-02-08 17:52:56 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
+import 'package:api_tutorial/event_repository.dart';
 import 'package:api_tutorial/connpass_repository.dart';
 
 Future<ConnpassRepository> fetchConnpassRepository() async {
